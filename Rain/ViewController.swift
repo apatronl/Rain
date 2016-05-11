722468e7aef6c08a58a0726ca441ab3c11aebebe
apatronl <apl7@gatech.edu>
2016-05-10 23:58:30 -0500

    Initial Commit

---
+++ Rain/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rain
+//
+//  Created by Alejandrina Patron on 5/10/16.
+//  Copyright © 2016 Ale Patrón. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
